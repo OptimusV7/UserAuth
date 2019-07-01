809bd3f81d2bfaf217ba95f94069e3cf9488cd1c
OptimusV7 <manstill09@outlook.com>
2019-07-01 09:18:11 +0300

    done with JWT

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -41,6 +41,15 @@ export class UserService {
       Password: this.formModel.value.Passwords.Password
     };
     return this.http.post(this.BaseURI + '/ApplicationUser/Register', body);
+  }
+
+  login(formData) {
+    return this.http.post(this.BaseURI + '/ApplicationUser/Login', formData);
+  }
+
+  getUserprofile(){
+    
+    return this.http.get(this.BaseURI+'/userProfile');
   }
 
 
